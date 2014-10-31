65ef38a92c3b43f908fb5170efe0b3f7a8056f99
Khalid Zuberi <kzuberi@gmail.com>
2014-10-31 19:16:17 -0400

    network metadata and naming

---
+++ builder/fetch_pubmed_metadata.py
@@ -0,0 +1,146 @@
+
+import argparse, os
+import pandas as pd
+import numpy as np
+from itertools import zip_longest
+from Bio import Entrez
+
+MAX_IDS_PER_REQUEST = 200
+
+PUBMED_FIELDS = ['pubmed_id', 'title', 'journal', 'journal_short',
+                 'year', 'first_author', 'last_author']
+
+def fetch(pmid_list, email=None):
+
+    Entrez.email = email
+    pmid_list = [pmid for pmid in pmid_list if pmid is not None]
+    print('retrieving', len(pmid_list))
+    print(pmid_list)
+    ids = ','.join(str(id) for id in pmid_list)
+    handle = Entrez.efetch(db='pubmed', id=ids, retmode='xml')
+    result = Entrez.read(handle)
+    handle.close()
+
+    return result
+
+
+def to_series(metadata):
+    '''
+    extract needed pieces from the metadata hierarchy
+    into a nice flat list (pandas Series with field names
+    in the index)
+    '''
+
+    pubmed_id = str(metadata['MedlineCitation']['PMID'])
+
+    # publication year
+    article = metadata['MedlineCitation']['Article']
+    pubdate = article['Journal']['JournalIssue']['PubDate']
+
+    try:
+        year = str(pubdate['Year'])
+    except KeyError as e:
+        medline_date = str(pubdate['MedlineDate'])
+        year = medline_date.split(' ')[0]
+
+    # article title
+    title = str(article['ArticleTitle'])
+
+    # authors ... needs handling for CollectiveName tag?
+    authors = article['AuthorList']
+
+    first_author = str(authors[0]['LastName'])
+
+    if len(authors) > 1:
+        last_author = str(authors[-1]['LastName'])
+    else:
+        last_author = ''
+
+    # journal
+    journal = str(article['Journal']['Title'])
+    journal_short = str(metadata['MedlineCitation']['MedlineJournalInfo']['MedlineTA'])
+
+    # return series
+    return pd.Series([pubmed_id, title, journal, journal_short,
+                      year, first_author, last_author],
+                     index=PUBMED_FIELDS)
+
+
+def cache_filename(cachedir, pubmed_id):
+    return os.path.join(cachedir, "%s.cfg" % pubmed_id)
+
+
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def main(inputfile, outputfile, pubmed_datafile, fetchsize):
+
+    network_metadata = pd.read_csv(inputfile, sep='\t', header=0, index_col=0,
+                                   na_filter=False, encoding='UTF8')
+
+    if os.path.exists(pubmed_datafile):
+        pubmed_data = pd.read_csv(inputfile, sep='\t', header=0, na_filter=False,
+                                  encoding='UTF8')
+    else:
+        pubmed_data = pd.DataFrame(columns=PUBMED_FIELDS)
+
+    # networks for which we need metadata
+    pmids = network_metadata[-network_metadata['pubmed_id'].isin(pubmed_data['pubmed_id'])]
+    pmids = pmids['pubmed_id'].copy()
+    pmids.replace('', np.nan, inplace=True)
+    pmids.dropna(inplace=True)
+    pmids.drop_duplicates(inplace=True)
+
+    if len(pmids) > 0:
+        # fetch rest from pubmed, in groups of fetchsize
+        groups = grouper(pmids, fetchsize)
+
+        all_fetched = []
+        for group in groups:
+            parsed_metadata = fetch(group)
+
+            fetched = [to_series(item) for item in parsed_metadata]
+            all_fetched.extend(fetched)
+
+
+        # glue the series together into a dataframe
+        new_pubmed_data = pd.concat(all_fetched, axis=1).transpose()
+
+        # update previously retrieved metadata
+        pubmed_data = pd.concat([pubmed_data, new_pubmed_data])
+        pubmed_data.to_csv(pubmed_datafile, sep='\t', header=True, index=False,
+                           na_filter=False, encoding='UTF8')
+
+    # join with network metadata
+    # TODO: check this preserves index (id) in the left dataframe
+    updated_network_metadata = pd.merge(network_metadata, pubmed_data,
+                                        on='pubmed_id', how='left')
+
+    updated_network_metadata.to_csv(outputfile, sep='\t', header=True, index=True,
+                                    na_filter=False, encoding='UTF8')
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='fetch article metadata from pubmed')
+
+    parser.add_argument('inputfile', help='input network metadata file')
+    parser.add_argument('outputfile', help='output network metadata file')
+    parser.add_argument('pubmed_data', help='file containing previously retrieved pubmed data, to be ')
+    parser.add_argument('--fetchsize', help='# of pubmed entries to retrieve in a single fetch, default 200',
+                        default=200)
+
+    args = parser.parse_args()
+    main(args.inputfile, args.outputfile, args.pubmed_data, args.fetchsize)
+
+
+
+
+
+
+
---
+++ builder/generate_network_name.py
@@ -0,0 +1,106 @@
+
+
+import argparse, os
+import pandas as pd
+import numpy as np
+
+DEDUP_DISCLAIMER = 'One of %s datasets produced from this publication.'
+ENDINGS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def format_author_info(first_author, last_author, year):
+    '''
+        First-Last-Year, with missing parts removed
+    '''
+
+    parts = [first_author, last_author, year]
+    parts = [str(part) for part in parts if bool(part) and part is not np.nan]
+    return '-'.join(parts)
+
+
+def name_selector(row):
+    if row['name'] and row['name'] is not np.nan:
+        return row['name']
+    elif row['auto_name'] and row['auto_name'] is not np.nan:
+        return row['auto_name']
+    else:
+        return row['name_from_file']
+
+
+def subgroup_auxer(subgroup_frame):
+    if len(subgroup_frame) == 1: #or subgroup_key == "":
+        return subgroup_frame
+
+    num_pubmid_matches = len(subgroup_frame)
+    aux = DEDUP_DISCLAIMER % num_pubmid_matches
+    subgroup_frame['aux_description'] = aux
+    return subgroup_frame
+
+
+def deduper(group_frame):
+
+        group_frame['dedup_suffix'] = ''
+        group_frame['aux_description'] = ''
+
+        if len(group_frame) < 2:
+            return group_frame
+
+        group_frame = group_frame.sort(['pubmed_id', 'dataset_key'], axis=0)
+
+        if len(group_frame) > len(ENDINGS):
+            raise Exception("too many colliding networks to rename")
+
+        # append the dedup letters 'A', 'B', etc to the name
+        group_frame['dedup_suffix'] = [' ' + ending for ending in ENDINGS[:len(group_frame)]]
+        group_frame['selected_name'] = group_frame['selected_name'] \
+                                           + group_frame['dedup_suffix']
+
+        # add extra text when the pmids match
+        subgroups = group_frame.groupby('pubmed_id')
+        return subgroups.apply(subgroup_auxer)
+
+
+def main(inputfile, outputfile):
+
+    network_metadata = pd.read_csv(inputfile, sep='\t', header=0, index_col=0, encoding='UTF8')
+
+    # new cols for network naming steps
+    #network_metadata['auto_name'] = ''
+    network_metadata['name_from_file'] = ''
+    network_metadata['selected_name'] = ''
+    network_metadata['final_name'] = ''
+
+    # generate auto name from pubmed metadata
+    network_metadata['auto_name'] = network_metadata.apply(lambda row:
+                                                           format_author_info(row['first_author'],
+                                                                              row['last_author'],
+                                                                              row['year']),
+                                                           axis=1)
+
+    # also a name based on the file
+    network_metadata['name_from_file'] = network_metadata.apply(lambda row:
+                                                                os.path.splitext(os.path.basename(row['dataset_key']))[0],
+                                                                axis=1)
+
+    # choose a name, either the name provided by user,
+    # else the name from pubmed, else the filename
+    network_metadata['selected_name'] = network_metadata.apply(lambda row: name_selector(row), axis=1)
+
+    # take care of name collisions
+    groups = network_metadata.groupby(['group', 'selected_name'])
+    updated_network_metadata = groups.apply(deduper)
+
+    # write out update
+    updated_network_metadata.to_csv(outputfile, sep='\t', header=True, index=True, encoding='UTF8')
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='compute network names')
+
+    parser.add_argument('inputfile', help='input network metadata file')
+    parser.add_argument('outputfile', help='output network metadata file')
+
+    args = parser.parse_args()
+    main(args.inputfile, args.outputfile)
+
+
