4d94c8172fa9cb58ba2ec96b0be7a7c53807739b
risserlin <ruth.isserlin@utoronto.ca>
2020-06-26 00:57:51 +0000

    added full path of lib destination for the jar files

--- scripts/all.sh
+++ scripts/all.sh
@@ -28,7 +28,7 @@ fi
 
 git clone https://github.com/GeneMANIA/pipeline.git merged
 pushd merged
-cp /home/gmbuild/dev/${build_version}/src/loader/target/*.jar lib
+cp /home/gmbuild/dev/${build_version}/src/loader/target/*.jar ~/sm_build_org/lib/
 cp /home/gmbuild/dev/${build_version}/src/loader/target/*.jar ~/pipeline/lib
 snakemake -j4 --config merge=1 orgs=../arabidopsis,../worm,../fly,../human,../mouse,../yeast,../rat,../zebrafish,../ecoli
 cp -r result/* /gm/db_build/${build_version}/
